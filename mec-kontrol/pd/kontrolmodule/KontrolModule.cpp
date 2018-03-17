ca3e9ec5a2285b25cba49080450a32daf485d9bc
Mark Harris <mharris@technobear.com>
2018-03-17 15:36:30 +0000

    fix issue with KM external on organelle

--- mec-kontrol/pd/kontrolmodule/KontrolModule.cpp
+++ mec-kontrol/pd/kontrolmodule/KontrolModule.cpp
@@ -9,9 +9,9 @@ static t_class *KontrolModule_class;
 
 typedef struct _KontrolModule {
     t_object x_obj;
-    Kontrol::EntityId rackId;
-    Kontrol::EntityId moduleId;
-    std::string moduleType;
+    t_symbol* rackId;
+    t_symbol* moduleId;
+    t_symbol* moduleType;
 } t_KontrolModule;
 
 
@@ -27,6 +27,7 @@ void KontrolModule_loaddefinitions(t_KontrolModule *x, t_symbol *defs);
 void KontrolModule_free(t_KontrolModule *x) {
 }
 
+#include <iostream>
 void *KontrolModule_new(t_symbol *name, t_symbol *type) {
     t_KontrolModule *x = (t_KontrolModule *) pd_new(KontrolModule_class);
     if (name && name->s_name && type && type->s_name) {
@@ -39,9 +40,11 @@ void *KontrolModule_new(t_symbol *name, t_symbol *type) {
                                                          moduleId, moduleType,
                                                          moduleType);
             rack->dumpParameters();
-            x->rackId = rackId;
-            x->moduleId = moduleId;
-            x->moduleType = moduleType;
+            std::cerr << "after dump" <<  rackId <<  " : " << moduleId << ":" << moduleType << std::endl;
+            x->rackId = gensym(rackId.c_str());
+            x->moduleId = gensym(moduleId.c_str());
+            x->moduleType = gensym(moduleType.c_str());
+            std::cerr << "success" << std::endl;
         } else {
             post("cannot create %s : No local rack found, KontrolModule needs a KontrolRack instance", name->s_name);
         }
@@ -65,7 +68,7 @@ void KontrolModule_setup(void) {
 void KontrolModule_loaddefinitions(t_KontrolModule *x, t_symbol *defs) {
     if (defs != nullptr && defs->s_name != nullptr && strlen(defs->s_name) > 0) {
         std::string file = std::string(defs->s_name);
-        Kontrol::KontrolModel::model()->loadModuleDefinitions(x->rackId, x->moduleId, file);
+        Kontrol::KontrolModel::model()->loadModuleDefinitions(x->rackId->s_name, x->moduleId->s_name, file);
     }
 }
 
