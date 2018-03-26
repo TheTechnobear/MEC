d733f3c797c4f8ceee78fe150b386951cb0bb39e
Giedrius <giedrius@blokas.io>
2018-03-26 03:59:39 +0300

    Set WINDOWS_EXPORT_ALL_SYMBOLS to true in CMakeLists.txt where necessary for Visual Studio builds.
    It enables cmake step to scan and force export of the symbols it finds. However, global variables
    don't get exported automatically, so manual treatment is necessary.

--- mec-kontrol/api/ChangeSource.h
+++ mec-kontrol/api/ChangeSource.h
@@ -29,9 +29,19 @@ private:
 bool operator==(const ChangeSource& a,const ChangeSource& b);
 bool operator!=(const ChangeSource &a, const ChangeSource &b);
 
-extern const  ChangeSource CS_LOCAL;
-extern const  ChangeSource CS_MIDI;
-extern const  ChangeSource CS_PRESET;
+#ifdef _MSC_VER
+#   ifdef mec_kontrol_api_EXPORTS
+#       define MEC_KONTROL_API __declspec(dllexport)
+#	else
+#       define MEC_KONTROL_API __declspec(dllimport)
+#   endif
+#else
+#   define MEC_KONTROL_API
+#endif
+
+extern MEC_KONTROL_API const  ChangeSource CS_LOCAL;
+extern MEC_KONTROL_API const  ChangeSource CS_MIDI;
+extern MEC_KONTROL_API const  ChangeSource CS_PRESET;
 
 } // namespace
 
